baffe64a638ec7700a7703ced66be14ffc03fe61
Andrew Chang <acchang4@gmail.com>
2022-11-12 16:52:49 -0500

    routers and models in

---
+++ models/schemas.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+let menuSchema = new mongoose.Schema({
+    name: (type:String, require: true),
+    Icon: (type:String, require: true),
+    menuUrl: (type:String, require: true),
+    entryDate: (type:Date, default: Date.now)
+})
+
+let userSchema = new mongoose.Schema({
+    email: (type:String, require: true),
+    pwd: (type:String, require: true),
+    entryDate: {type:Date,default:Date.now}
+})
+
+let menu = mongoose.model('menu', menuSchema, 'menu')
+let users = mongoose.model('users', userSchema, 'users')
+
+let mySchemas = {
+    'menu' : menu,
+    'users' : users
+}
+
+module.exports = mySchemas
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+const indexController = require(`../controllers/index`)
+
+// const { ensureAuth, ensureGuest } = require('../middleware/auth')
+
+router.get('/', indexController.getHome)
+router.post('/q', indexController.getSearch)
+
+module.exports = router
