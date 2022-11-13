79a3e89b974c619ad01f16d4da8a8ec6e1dc5e14
Andrew Chang <acchang4@gmail.com>
2022-11-12 22:24:05 -0500

    final

---
+++ README.md
@@ -0,0 +1,9 @@
+Mayanwolfe's restaurant tracker
+
+Built to follow this tutorial:
+
+https://www.youtube.com/watch?v=T5-T8_XZNGQ
+
+Original code:
+https://github.com/Mayanwolfe/restaurant_menu_tracker
+
--- models/schemas.js
+++ models/schemas.js
@@ -1,24 +1,21 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
 
-let menuSchema = new mongoose.Schema({
-    name: {type: String, require: true},
-    Icon: {type: String, require: true},
-    menuUrl: {type: String, require: true},
-    entryDate: {type: Date, default: Date.now}
-})
+let menuSchema = new schema({
+    name: {type:String, require:true},
+    icon: {type:String, require:true},
+    menuUrl: {type:String, require:true},
+    entryDate: {type:Date, default:Date.now}
+});
 
-let userSchema = new mongoose.Schema({
-    email: {type: String, require: true},
-    pwd: {type: String, require: true},
-    entryDate: {type: Date,default:Date.now}
-})
+let usersSchema = new schema({
+    email: {type:String, require:true},
+    pwd: {type:String, require:true},
+    entryDate: {type:Date, default:Date.now}
+});
 
-let menu = mongoose.model('menu', menuSchema, 'menu')
-let users = mongoose.model('users', userSchema, 'users')
+let menu = mongoose.model('menu', menuSchema, 'menu');
+let users = mongoose.model('users', usersSchema, 'users');
+let mySchemas = {'menu':menu, 'users':users};
 
-let mySchemas = {
-    'menu' : menu,
-    'users' : users
-}
-
-module.exports = mySchemas
+module.exports = mySchemas;
