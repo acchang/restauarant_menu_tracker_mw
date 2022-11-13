fe7b970b8ccf2b50b5aeb8897419c1ac33495851
Andrew Chang <acchang4@gmail.com>
2022-11-12 21:48:01 -0500

    runs

--- models/schemas.js
+++ models/schemas.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose')
 
 let menuSchema = new mongoose.Schema({
-    name: (type:String, require: true),
-    Icon: (type:String, require: true),
-    menuUrl: (type:String, require: true),
-    entryDate: (type:Date, default: Date.now)
+    name: {type: String, require: true},
+    Icon: {type: String, require: true},
+    menuUrl: {type: String, require: true},
+    entryDate: {type: Date, default: Date.now}
 })
 
 let userSchema = new mongoose.Schema({
-    email: (type:String, require: true),
-    pwd: (type:String, require: true),
-    entryDate: {type:Date,default:Date.now}
+    email: {type: String, require: true},
+    pwd: {type: String, require: true},
+    entryDate: {type: Date,default:Date.now}
 })
 
 let menu = mongoose.model('menu', menuSchema, 'menu')
--- server.js
+++ server.js
@@ -3,18 +3,18 @@ const app = express()
 const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
+const MongoStore = require('connect-mongo')
 const flash = require('express-flash')
 const logger = require('morgan')
 const connectDB = require('./config/database')
-const indexRouter = require('./routes/index')
-const menuRouter = require('./routes/menu')
-const loginRouter = require('./routes/login')
+const indexRouter = require('./routes/index');
+const menuRouter = require('./routes/menu');
+const loginRouter = require('./routes/login');
 
 require('dotenv').config({path: './config/.env'})
 
 // Passport config
-// require('./config/passport')(passport)
+//require('./config/passport')(passport)
 
 connectDB()
 
@@ -29,20 +29,20 @@ app.use(
       secret: 'keyboard cat',
       resave: false,
       saveUninitialized: false,
-      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+      store: MongoStore.create({mongoUrl: process.env.DB_STRING}),
     })
-  )
+  );
   
 // Passport middleware
 // app.use(passport.initialize())
 // app.use(passport.session())
 
 app.use(flash())
-
-app.use('/', indexRouter)
-app.use('/menu', menuRouter)
-app.use('/login', loginRouter)
-
+  
+app.use('/', indexRouter);
+app.use('/menu', menuRouter);
+app.use('/login', loginRouter);
+ 
 app.listen(process.env.PORT, ()=>{
     console.log('Server is running, you better catch it!')
 })    
